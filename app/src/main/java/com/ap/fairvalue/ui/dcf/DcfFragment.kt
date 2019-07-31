a858c4e10a032ded68d472dc1c4a10ba896d67e8
Maklakov Sergey <maklakov.sergey@hotmail.com>
2019-07-31 15:24:10 +0500

    настроены биндинги в dcf

--- app/src/main/java/com/ap/fairvalue/ui/dcf/DcfFragment.kt
+++ app/src/main/java/com/ap/fairvalue/ui/dcf/DcfFragment.kt
@@ -4,26 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.ap.fairvalue.R
+import com.ap.fairvalue.databinding.DcfFragmentBinding
 
 
 class DcfFragment : Fragment() {
 
     private lateinit var viewModel: DcfViewModel
+    private lateinit var binding: DcfFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.dcf_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.dcf_fragment, container, false);
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(DcfViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.model = viewModel
+
+//        btnDcfCalc.setOnClickListener {
+//
+//            viewModel.calc()
+//            binding.invalidateAll()
+//        }
     }
 
 }
--- app/src/main/java/com/ap/fairvalue/ui/dcf/DcfViewModel.kt
+++ app/src/main/java/com/ap/fairvalue/ui/dcf/DcfViewModel.kt
@@ -1,7 +1,18 @@
 package com.ap.fairvalue.ui.dcf
 
+import androidx.databinding.ObservableDouble
 import androidx.lifecycle.ViewModel;
 
 class DcfViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val earningPerShare = ObservableDouble(2.43)
+    val growthRate5 = ObservableDouble(4.0)
+    val growthRate10 = ObservableDouble(4.0)
+    val growthRate11 = ObservableDouble(1.0)
+    val discountRate = ObservableDouble(5.0)
+    val fairValue = ObservableDouble(0.0)
+
+    fun calc() {
+        fairValue.set(earningPerShare.get())
+    }
 }
